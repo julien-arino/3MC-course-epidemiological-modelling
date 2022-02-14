2ee07b380058844191fd985d1afcd60ba613bdc2
Julien Arino <julien.arino@gmail.com>
2022-02-14 12:58:01 -0600

    Script to install required libraries

---
+++ CODE/install_required_libraries.R
@@ -0,0 +1,37 @@
+# Install (and load) the libraries required for running the code in this repository.
+# 
+# This script works best if no library has yet been loaded, so run from a fresh session or call
+# from the command line. For example, in Linux:
+# Rscript install_required_libraries.R
+# 
+# In Linux, you may also want to run the script as sudo so that it is installed for all users. 
+# In this case, the command line would be
+# sudo Rscript install_required_libraries.R
+
+required_liraries = c(
+  "countrycode",
+  "deSolve",
+  "formatR",
+  "GA",
+  "geosphere",
+  "htmltab",
+  "ISOweek",
+  "kableExtra",
+  "knitr",
+  "lubridate",
+  "magrittr",
+  "maps",
+  "mapdata",
+  "measurements",
+  "methods",
+  "sqldf",
+  "wbstats",
+  "XML"
+)
+
+for (l in required_liraries) {
+  installed = require(l)
+  if (!installed) {
+    install.packages(l)
+  }
+}
