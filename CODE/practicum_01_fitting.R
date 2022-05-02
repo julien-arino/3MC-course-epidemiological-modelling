e57e0271d9d67664859fff0827fcc5392b285863
Julien Arino <julien.arino@gmail.com>
2022-05-02 11:00:50 -0500

    Update fitting code

--- CODE/practicum_01_fitting.R
+++ CODE/practicum_01_fitting.R
@@ -7,8 +7,8 @@ library(GA)
 # The RHS function for KMK
 RHS_KMK_SIR <- function(t, x, p) {
   with(as.list(c(x,p)), {
-    dS <- -beta*S*I
-    dI <- beta*S*I-gamma*I
+    dS <- -beta*S*I/pop
+    dI <- beta*S*I/pop-gamma*I
     dR <- gamma*I
     return(list(c(dS, dI, dR)))
   })
@@ -16,27 +16,43 @@ RHS_KMK_SIR <- function(t, x, p) {
 
 # Compute the error based on one parameter value. Will also need a few more
 # arguments
-error_Newton <- function(p_vary, params, incidence_data) {
+error_incidence <- function(p_vary, 
+                            params, 
+                            incidence_data,
+                            method = "rk4",
+                            which_incidence = "inc100") {
   # Anything that changes during optimisation needs to be set here
-  params$beta = p_vary[1]
-  IC = c(S = params$pop, I = incidence_data$inc[1], R = 0)
+  params$beta = as.numeric(p_vary["beta"])
+  params$gamma = as.numeric(p_vary["gamma"])
+  # Check value of R0. If it is less than 1, no need to compute ODE solution,
+  # just return Inf
+  R0 = params$beta / params$gamma
+  if (R0<1) {
+    return(Inf)
+  }
+  # I0 is the I that gives us the incidence we are matching in the data. Since
+  # incidence=beta*S*I/N, we have
+  # I(0) = incidence*N/(beta*S(0)) ~= incidence/beta
+  S0 = params$pop
+  I0 = data_subset[[which_incidence]][1]/(params$beta)
+  IC = c(S = S0, I = I0, R = 0)
+  # The times at which we compute the solution to compare with data
   times = as.numeric(as.Date(incidence_data$date))
-  sol = ode(IC, times, RHS_KMK_SIR, params,
-            method = "rk4")
+  sol = ode(IC, times, RHS_KMK_SIR, params, method = method)
   # Error checking
   if (sol[dim(sol)[1],"time"] < times[length(times)]) {
-    writeLines("Cut short")
+    #writeLines("Cut short")
     return(Inf)
   }
   # Values required to compute the error
-  incidence_from_run = params$beta * sol[,"S"] * sol[,"I"]
+  incidence_from_run = params$beta * sol[,"S"] * sol[,"I"] / params$pop
   # Compute the error
-  diff_values = incidence_data$inc100 - incidence_from_run
+  diff_values = incidence_data[[which_incidence]] - incidence_from_run
   diff_values_squared = diff_values^2
   error = sum(diff_values_squared)
   return(error)
 }
-  
+
 # Read in the data for France
 data = readr::read_csv("https://www.sentiweb.fr/datasets/incidence-PAY-3.csv",
                        skip = 1)
@@ -61,79 +77,77 @@ data$date[idx] = ymd(paste0(data$year[idx],"-12-31"))
 data = data %>%
   arrange(date)
 
+# Type of incidence to use (column name in the data)
+which_incidence = "inc"
+
 # Let us now select one epidemic season (mid year to next). Use another variable
 # so we don't have to reload the data if we do weird edits..
 # We take a flu season prior to the pandemic (they are weird after that)
 beg_year = 2018
 end_year = 2019
 data_subset = data %>%
-  filter(date > paste0(beg_year,"-07-01")) %>%
-  filter(date <= ymd(paste0(end_year,"-06-30")))
+  filter(date > paste0(beg_year,"-12-15")) %>%
+  filter(date <= ymd(paste0(end_year,"-04-30")))
 
-plot(data_subset$date, data_subset$inc100,
+plot(data_subset$date, data_subset[[which_incidence]],
      type = "b",
-     xlab = "Month", ylab = "Incidence / 100K",
+     xlab = "Month", ylab = "Incidence",
      main = paste0(beg_year, "-", end_year, " flu season"))
 
 # To do work, we will need the total population of France
 pop_data_FRA <- wb_data(country = "FRA", indicator = "SP.POP.TOTL",
-                         mrv = 100, return_wide = FALSE) %>%
+                        mrv = 100, return_wide = FALSE) %>%
   filter(date == beg_year)
-# For numerical reasons, we work per 100K people..
-pop_data_FRA = pop_data_FRA$value / 100000
+# For numerical reasons, we might work per 100K people.. Detect this.
+if (grepl("100", which_incidence)) {
+  pop_data_FRA = pop_data_FRA$value / 100000
+} else {
+  pop_data_FRA = pop_data_FRA$value
+}
 
 params = list()
-params$gamma = 1/3.5/7   # Let's see if we can fit with this simple value
-params$beta = 1.5*params$gamma/pop_data_FRA
+params$gamma = 1/3.5   # Let's see if we can fit with this simple value
+# R0=beta/gamma, so beta=R0*gamma
+params$beta = 1.5*params$gamma
 # Add population data to this, for convenience
 params$pop = pop_data_FRA
 
-IC = c(S = pop_data_FRA, I = data_subset$inc100[1], R = 0)
-times = as.numeric(as.Date(data_subset$date))
-
-sol <- ode(IC, times, RHS_KMK_SIR, params)
-
-# Just to check: does the error function indeed return what we want it to return
-# and does it not change beta (here)
-params$beta
-error_Newton(p_vary = c(beta=0.0001), 
-             params = params, 
-             incidence_data = data_subset)
-params$beta
-
-# "Classic" minimisation using a Newton-type algorithm
-param_value = nlm(f = error_Newton,
-                  p = params$beta,
-                  params = params, incidence_data = data_subset) 
-# "Classic" minimisation using other classic algorithm
-param_value = optim(fn = error_Newton,
-                    par = params$beta,
-                    params = params, incidence_data = data_subset) 
-# "Classic" minimisation using other classic algorithm
-param_value = optim(fn = 
-                      function(x) error_Newton(x, 
-                                               params = params,
-                                               incidence_data = data_subset),
-                    par = c(params$beta)) 
-# Using a genetic algorithm
+## Fit using a genetic algorithm
 GA = ga(
   type = "real-valued",
-  fitness = function(x) -error_Newton(p_vary = x,
-                                      params = params,
-                                      incidence_data = data_subset),
+  fitness = function(x) -error_incidence(p_vary = c(beta = x[1], gamma = x[2]),
+                                         params = params,
+                                         incidence_data = data_subset,
+                                         which_incidence = which_incidence,
+                                         method = "rk4"),
   parallel = TRUE,
-  lower = c(0),
-  upper = c(0.1),
-  pcrossover = 0.7, 
-  pmutation = 0.3, 
-  suggestions = params$beta,
-  popSize = 1000,
-  maxiter = 500
+  lower = c(0.1, 1/20),
+  upper = c(1, 1/2),
+  pcrossover = 0.7,
+  pmutation = 0.2,
+  optim = TRUE,
+  optimArgs = list(method = "CG"),
+  suggestions = c(params$beta, params$gamma),
+  popSize = 500,
+  maxiter = 100
 )
 
-params$beta = GA@solution
-IC = c(S = pop_data_FRA, I = data_subset$inc100[1], R = 0)
-times = as.numeric(as.Date(data_subset$date))
+params$beta = GA@solution[1]
+params$gamma = GA@solution[2]
+S0 = pop_data_FRA
+I0 = data_subset[[which_incidence]][1]/(params$beta)
+IC = c(S = S0, I = I0, R = 0)
+dates_num = as.numeric(as.Date(data_subset$date))
+times = seq(dates_num[1], dates_num[length(dates_num)], 0.1)
 sol <- ode(IC, times, RHS_KMK_SIR, params)
-sol_incidence = as.numeric(params$beta) * sol[,"S"] * sol[,"I"]
-plot(sol[,"time"], sol_incidence)
+sol_incidence = as.numeric(params$beta) * sol[,"S"] * sol[,"I"]/params$pop
+y_max = max(max(sol_incidence), max(data_subset[[which_incidence]]))
+
+plot(sol[,"time"], sol_incidence, type = "l",
+     xlab = "Date", ylab = "Incidence",
+     xaxt = "n", lwd = 2, col = "red",
+     ylim = c(0, y_max))
+lines(as.numeric(as.Date(data_subset$date)), data_subset[[which_incidence]],
+      type = "b")
+dates_pretty = pretty(dates_num)
+axis(1, at = dates_pretty, labels = as.Date(dates_pretty, origin="1970-01-01"))
